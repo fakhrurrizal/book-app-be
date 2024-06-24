61e2c146c64224f72440311c6b4dd46aca8799e4
fakhrurrizal10 <fakhrurrizal10@gmail.com>
2024-06-24 21:58:52 +0700

    Revisi Pagination data

--- main.go
+++ main.go
@@ -4,21 +4,34 @@ import (
 	router "book-app/app/routers"
 	"book-app/config"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/labstack/echo/v4"
 	"gopkg.in/tylerb/graceful.v1"
 )
 
-func Main() {
+func main() {
+	app := Start()
+
+	app.Server.Addr = "127.0.0.1:" + config.LoadConfig().Port
+	log.Printf("Server: " + config.LoadConfig().BaseUrl)
+
+	graceful.ListenAndServe(app.Server, 5*time.Second)
+}
+
+func Main(w http.ResponseWriter, r *http.Request) {
+	e := Start()
+
+	e.ServeHTTP(w, r)
+}
+
+func Start() *echo.Echo {
 	app := echo.New()
 
 	config.Database()
 
 	router.Init(app)
 
-	app.Server.Addr = "127.0.0.1:" + config.LoadConfig().Port
-	log.Printf("Server: " + config.LoadConfig().BaseUrl)
-
-	graceful.ListenAndServe(app.Server, 5*time.Second)
+	return app
 }
