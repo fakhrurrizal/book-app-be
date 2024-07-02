934f4a448649201c957725dbff1fea59a18815ff
fakhrurrizal10 <fakhrurrizal10@gmail.com>
2024-07-02 07:46:37 +0700

    update revisi deploy server 1

--- app/middlewares/cors.go
+++ app/middlewares/cors.go
@@ -9,6 +9,7 @@ import (
 func Cors() echo.MiddlewareFunc {
 	return middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{"X-Requested-With", "Content-Type", "Authorization"},
 		AllowMethods: []string{echo.HEAD, echo.GET, echo.PUT, echo.POST, echo.DELETE},
 	})
 }
